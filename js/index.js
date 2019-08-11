6cda01ff5308d81aba7a9886ffd3479d1121f6ba
Ruth Poliakon <r.poliakon@gmail.com>
2019-08-11 17:31:59 -0600

    img mouseover and mouseout events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+// image mouseover and mouseout events
+const contentImages = document.querySelectorAll("img");
+
+function imgOpacity(event) {
+  event.currentTarget.style.transition = "all .5s";
+  event.currentTarget.style.opacity = "0.5";
+}
+function imgOpacityRemove(event) {
+  event.currentTarget.style.transition = "all .5s";
+  event.currentTarget.style.opacity = "1";
+}
+
+contentImages.forEach(img => {
+  img.addEventListener("mouseover", imgOpacity);
+  img.addEventListener("mouseout", imgOpacityRemove);
+});
