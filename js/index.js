407b4c22a904f581ef83ca6da07d6e1bcd68605c
Ruth Poliakon <r.poliakon@gmail.com>
2019-08-11 18:24:08 -0600

    destination mouseover, mouseout, mousedown, mouseup, and stopPropagation events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Your code goes here
-// image mouseover and mouseout events
+// image mouseenter and mouseleave events
 const contentImages = document.querySelectorAll("img");
 
 function imgOpacity(event) {
@@ -12,6 +12,49 @@ function imgOpacityRemove(event) {
 }
 
 contentImages.forEach(img => {
-  img.addEventListener("mouseover", imgOpacity);
-  img.addEventListener("mouseout", imgOpacityRemove);
+  img.addEventListener("mouseenter", imgOpacity);
+  img.addEventListener("mouseleave", imgOpacityRemove);
 });
+
+// destination mouseover, mouseout events, mousedown, mouseup events and stopPropagation
+const destinationContainer = document.querySelectorAll(".destination");
+const destinationButton = document.querySelectorAll(".destination .btn");
+
+function enlarge(event) {
+  event.currentTarget.style.transition = "all .5s";
+  event.currentTarget.style.transform = "scale(1.1, 1.1)";
+}
+
+function enlargeRemove(event) {
+  event.currentTarget.style.transition = "all .5s";
+  event.currentTarget.style.transform = "scale(1, 1)";
+}
+
+function btnNormal(event) {
+  event.stopPropagation();
+  event.currentTarget.style.boxShadow = "0 6px #138597";
+}
+
+function btnHover(event) {
+  event.stopPropagation();
+  event.currentTarget.style.boxShadow = "0 4px #138597";
+}
+
+function btnPush(event) {
+  event.stopPropagation();
+  event.currentTarget.style.boxShadow = "0 0 #138597";
+}
+
+destinationContainer.forEach(div => {
+  div.addEventListener("mouseover", enlarge);
+  div.addEventListener("mouseout", enlargeRemove);
+});
+
+destinationButton.forEach(button => {
+  button.addEventListener("mouseover", btnHover);
+  button.addEventListener("mouseout", btnNormal);
+  button.addEventListener("mousedown", btnPush);
+  button.addEventListener("mouseup", btnNormal);
+});
+
+// header scroll event
